e9cd7eb95eabce0218f68a575e09ed00e3e34188
Gokulnath T <jeevas@dotsolved.com>
2023-09-25 15:28:05 +0530

    initial commit

---
+++ android/src/main/java/com/mylibrary/MyLibraryModule.java
@@ -0,0 +1,32 @@
+package com.mylibrary;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = MyLibraryModule.NAME)
+public class MyLibraryModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "MyLibrary";
+
+  public MyLibraryModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
